1a0862dceb47df3002566a7ef18b4346367defe0
Dinesh <dinesh.wasnik@i-serve.com.my>
2021-07-28 02:02:37 +0800

    Added passport

--- database/migrations/2021_07_25_192008_create_loan_table.php
+++ database/migrations/2021_07_25_192008_create_loan_table.php
@@ -18,6 +18,7 @@ class CreateLoanTable extends Migration
              $table->integer('user_id')->unsigned();
              $table->integer('amount')->unsigned();
              $table->enum('term', array('3','6','9', '12'))->default('3');
+             $table->integer('installment')->unsigned()->default('3');
              $table->boolean('is_approved')->nullable();
              $table->timestamps();
         });
